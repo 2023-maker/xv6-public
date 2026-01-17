efd8dcda0371b8bf67bb85f3974ec84dc3faf54c
党祖鑫 <dang@WIN-RUQSAJKULJD.localdomain>
2026-01-17 23:30:12 +0800

    添加了部分注释，不改变程序和功能

--- bootmain.c
+++ bootmain.c
@@ -11,9 +11,12 @@
 #include "memlayout.h"
 
 #define SECTSIZE  512
+// 磁盘扇区大小固定为512字节
 
+// 函数声明：从磁盘读取指定扇区到内存指定地址
 void readseg(uchar*, uint, uint);
-
+// bootmain是xv6启动流程中第一个执行的C语言函数
+// 被boot/bootasm.S中的start32函数调用，运行在32位保护模式下
 void
 bootmain(void)
 {
@@ -21,16 +24,19 @@ bootmain(void)
   struct proghdr *ph, *eph;
   void (*entry)(void);
   uchar* pa;
-
+// 1. 将内核的ELF文件头加载到内存地址0x10000处 (64KB位置)
   elf = (struct elfhdr*)0x10000;  // scratch space
-
+// 2. 从磁盘读取内核ELF文件头到内存：读取从磁盘扇区1开始的8个扇区
+  //    磁盘扇区0是bootasm.S引导扇区，内核从扇区1开始存储
   // Read 1st page off disk
   readseg((uchar*)elf, 4096, 0);
 
   // Is this an ELF executable?
   if(elf->magic != ELF_MAGIC)
     return;  // let bootasm.S handle error
-
+// 4. 遍历ELF文件的所有程序段头，加载内核的代码段和数据段到内存指定位置
+  //    elf->phoff 是程序段头在ELF文件中的偏移量
+  //    elf->phnum 是程序段的总个数
   // Load each program segment (ignores ph flags).
   ph = (struct proghdr*)((uchar*)elf + elf->phoff);
   eph = ph + elf->phnum;
